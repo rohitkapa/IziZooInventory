a291e36bf606d9469c305305df3b4ec7795ae56e
rohit <rohitkapa@gmail.com>
2016-03-23 23:45:03 -0400

    Added main class

--- src/main/scala/IziInventory.scala
+++ src/main/scala/IziInventory.scala
@@ -6,4 +6,10 @@
 //This application is designed to meet requirements of IZI zoo inventory management
 object IziInventory {
 
+  def main(args: Array[String]) {
+    while(true) {
+      println("What would you like to do? \n 1. Add Inventory \n 2. Add Feed details")
+    }
+  }
+
 }
